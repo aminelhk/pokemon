bae9295cd0f319f49c9f750bb67dc411241a19aa
aminelhk <amine.professionnel1@gmail.com>
2024-07-08 12:05:22 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import "./App.css";
 
 function App() {
   const [pokemon, setPokemon] = useState(null);
-  const [name, setName] = useState(""); // Ajouter un état pour stocker le nom
+  const [name, setName] = useState("");
 
-  // Fonction pour convertir le prénom en un nombre pseudo-aléatoire basé sur les caractères
   const getPokemonIndex = (name) => {
     let hash = 0;
     for (let i = 0; i < name.length; i++) {
       hash = (hash << 5) - hash + name.charCodeAt(i);
       hash = hash & hash;
     }
-    return Math.abs(hash % 151) + 1; // Assurer un nombre positif et dans la plage 1-151
+    return Math.abs(hash % 151) + 1;
   };
 
   const fetchPokemon = async () => {
