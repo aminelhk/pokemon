a85dbd41b43e79984f0f03fcea7d1186385533f7
aminelhk <amine.professionnel1@gmail.com>
2024-07-10 11:56:19 +0200

    a

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Quel Pokémon es-tua ?</p>
+        <p>Quel Pokémon es-tu ?</p>
         {pokemon && (
           <div>
             <h2>{pokemon.name}</h2>
